61f1c15e21fc728301b69fc73980e08ba99dcd3e
DT User6 <aleksa.car1997@gmail.com>
2022-06-01 12:46:21 +0200

    I hope this work, import images int ThreeImagejs

--- src/components/ThreeImg.js
+++ src/components/ThreeImg.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import image1 from '../img/pet-klub.jpg';
+import image2 from '../img/moj-ljubimac.jpg';
+import image3 from '../img/veterinar.jpg';
 
 const ThreeImg = () => {
   return (
    <div className='three-img'>
-       <a href='/pet-klub'><img src='../img/pet-klub.jpg' alt='klub'/></a>
-       <a href='/ljubimac'><img src='../img/moj-ljubimac.jpg' alt='ljubimac'/></a>
-       <a href='/veterinar'><img src='../img/veterinar.jpg' alt='veterinar'/></a>
+       <a href='/pet-klub'><img src={image1} alt='klub'/></a>
+       <a href='/ljubimac'><img src={image2} alt='ljubimac'/></a>
+       <a href='/veterinar'><img src={image3} alt='veterinar'/></a>
    </div>
   )
 }
